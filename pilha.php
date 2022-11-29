1da399a387e9191f00abedbfa796473f7220217a
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-11-29 15:26:03 -0300

    att

--- pilha.php
+++ pilha.php
@@ -1,1 +1,17 @@
 <?php
+
+function funcao1()
+{
+    echo 'Entrei na função 1' . PHP_EOL;
+    
+    funcao2();
+    echo 'Saindo da função 1' . PHP_EOL;
+}
+
+function funcao2()
+{
+    echo 'Entrei na função 2' . PHP_EOL;
+    for ($i = 1; $i <= 5; $i++){
+        echo 'Salve';
+    }
+}
