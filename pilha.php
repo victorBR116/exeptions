5b516b6cb5f23333a62c6db5419f9e6dded4fcba
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-12-08 17:49:47 -0300

    att

--- pilha.php
+++ pilha.php
@@ -3,20 +3,24 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    try {
-    $arrayFixo = new SplFixedArray(2);
-    $arrayFixo[3] = 'Valor';
-} catch(RuntimeException $erro){
-    echo 'Erro esperado na função 1' . PHP_EOL;
-}
-// $numero = intdiv(5, 0);
+    try{
     funcao2();
+    }catch(RuntimeException | DivisionByZeroError $erroOuExcecao){
+        echo $erroOuExcecao->getMessage() . PHP_EOL;
+        echo $erroOuExcecao->getLine() . PHP_EOL;
+        echo $erroOuExcecao->getTraceAsString() . PHP_EOL;
+    }
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
+    //$numero = intdiv(5, 0);
+    $arrayFixo = new SplFixedArray(2);
+    $arrayFixo[3] = 'Valor';
+   
     for ($i = 1; $i <= 5; $i++){
         echo $i . PHP_EOL;
     }
