91a61ce54e9ae38ae48287b19a4e64cad32f4c7a
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-12-20 16:56:17 -0300

    att

--- dadosAgendamento.php
+++ dadosAgendamento.php
@@ -1,14 +1,41 @@
+<html>
+  <head>
+    <title>Agendamento</title>
+  </head>
+  <body>
+    <h1>Agendamento</h1>
+    <form method="post" action="processa_agendamento.php">
+      <label for="data">Data:</label><br>
+      <input type="date" id="data" name="data"><br>
+      <label for="hora">Hora:</label><br>
+      <input type="time" id="hora" name="hora"><br>
+      <label for="descricao">Descrição:</label><br>
+      <textarea id="descricao" name="descricao"></textarea><br>
+      <input type="submit" id="submit" name="submit" value="Agendar">
+    </form> 
+    
+  </body>
+</html>
+
 <?php
-//codigo do gpt
-require_once 'E:\Estudos\Alura\exceptionsAlura\agendamentogpt.html';
 // Pega a data do agendamento
-$data = $_POST['data'];
+if (isset($_POST['data'])) {
+  $data = $_POST['data'];
+}
 
 // Pega a hora do agendamento
-$hora = $_POST['hora'];
+if (isset($_POST['hora'])) {
+  $hora = $_POST['hora'];
+}
 
 // Pega a descrição do agendamento
-$descricao = $_POST['descricao'];
+if (isset($_POST['descricao'])) {
+  $descricao = $_POST['descricao'];
+}
+
+if (isset($_POST['submit'])) {
+    $descricao = $_POST['submit'];
+  }
 
 // Aqui você pode processar os dados do agendamento
 // Por exemplo, armazenar os dados em um banco de dados
